457be2e636569bb00badd07e359bd44df267969f
taaewoo <kauasltw@gmail.com>
2020-04-20 18:51:46 +0900

    Updated

--- 백준/17142/17142.cpp
+++ 백준/17142/17142.cpp
@@ -1,90 +1,103 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <algorithm>
 #include <string.h>
 using namespace std;
 
 #define FOR(x,n,m)  for(int x=n;x<(m);x++)
 
-typedef struct P{
+typedef struct V{
     int x,y,d;
-    P(int X, int Y, int D) : x(X), y(Y), d(D) {}
+    V(int X, int Y, int D) : x(X), y(Y), d(D) {}
 };
 
-int n;
-int arr[20][20];
-int sx,sy,ss=2,sh=0;
-bool visit[20][20];
+int n,m;
+int arr[50][50];
+bool visit[50][50];
 int dir[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};
-int ans = 0;
-
-void cal(vector<pair<int,int>> fish, int dd){
-    sort(fish.begin(),fish.end());
-    arr[sx][sy] = 0;
-    sx = fish[0].first;
-    sy = fish[0].second;
-    arr[sx][sy] = 0;
-    sh++;
-    if(sh == ss){
-        sh=0;
-        ss++;
-    }
-    ans += dd;
-}
+vector<V> virus;
+vector<V> active;
+int fAns = 987654321;
 
 bool bfs(){
-    int dd = -1;
-    memset(visit, false, sizeof(visit));
-    queue<P> q;
-    q.push(P(sx,sy,0));
-    visit[sx][sy] = true;
-    vector<pair<int,int>> fish;
+    memset(visit,false,sizeof(visit));
+    int temp_arr[50][50];
+    memmove(temp_arr,arr,sizeof(arr));
     
-    while(!q.empty()){
-        P cur = q.front(); q.pop();
-        
-        if(cur.d > dd && dd != -1) break;
-        
-        if(cur.d == dd && arr[cur.x][cur.y]!=0 && arr[cur.x][cur.y]!=ss) fish.push_back(make_pair(cur.x,cur.y));
-        
-        FOR(i,0,4){
-            int nx = cur.x+dir[i][0], ny = cur.y+dir[i][1];
-            if(nx<0||nx>=n||ny<0||ny>=n) continue;
-            if(visit[nx][ny]) continue;
-            if(arr[nx][ny]>ss) continue;
-            
-            if(arr[nx][ny]<ss && arr[nx][ny]!=0 && dd==-1) dd = cur.d+1;
-            
-            visit[nx][ny] = true;
-            q.push(P(nx,ny,cur.d+1));
-        }
+    queue<V> q;
+    FOR(i,0,active.size()){
+        q.push(V(active[i].x,active[i].y,0));
+        visit[active[i].x][active[i].y] = true;
     }
+    int ans = 0;
     
-    if(dd!=-1){
-        cal(fish,dd);
+    int blank = 0;
+    FOR(i,0,n)
+        FOR(j,0,n)
+            if(temp_arr[i][j]==0) blank++;
+    
+    if(blank==0){
+        fAns = 0;
         return true;
     }
-    else return false;
+    
+    while(!q.empty()){
+        V cur = q.front(); q.pop();
+        
+        bool check = false;
+        FOR(i,0,4){
+            int nx = cur.x + dir[i][0], ny = cur.y + dir[i][1];
+            if(nx<0||nx>=n||ny<0||ny>=n) continue;
+            if(visit[nx][ny] || temp_arr[nx][ny]==1) continue;
+            
+            visit[nx][ny] = true;
+            if(temp_arr[nx][ny]==0) blank--;
+            temp_arr[nx][ny] = 2;
+            q.push(V(nx,ny,cur.d+1));
+            ans = cur.d+1;
+            if(blank==0) check = true;
+        }
+        if(check) break;
+    }
+    
+    if(blank==0) fAns = min(fAns,ans);
+    
+    return true;
+}
+
+void dfs(int idx, int vN){
+    if(vN>=m){
+        bfs();
+        return;
+    }
+    
+    if(idx>=virus.size()) return;
+    
+    arr[virus[idx].x][virus[idx].y] = 2;
+    active.push_back(virus[idx]);
+    dfs(idx+1,vN+1);
+    arr[virus[idx].x][virus[idx].y] = -1;
+    active.pop_back();
+    
+    dfs(idx+1,vN);
 }
 
 int main() {
-    cin >> n;
+    cin >> n >> m;
     
     FOR(i,0,n){
         FOR(j,0,n){
             cin >> arr[i][j];
-            if(arr[i][j]==9){
-                sx = i;
-                sy = j;
+            if(arr[i][j]==2){
+                virus.push_back(V(i,j,0));
+                arr[i][j]=-1;
             }
         }
     }
     
-    while(1)
-        if(!bfs()) break;
+    dfs(0,0);
     
-    cout << ans;
+    cout << (fAns==987654321 ? -1:fAns);
     
     return 0;
 }
