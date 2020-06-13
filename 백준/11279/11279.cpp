a3c60fb25f9c7c00e19d290c5f9ab0e816cabb90
taaewoo <kauasltw@gmail.com>
2020-06-13 21:06:02 +0900

    Updated

---
+++ 백준/11279/11279.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+#define FOR(x,n,m)  for(int x=n;x<(m);x++)
+
+int n;
+priority_queue<int,vector<int>> pq;
+
+int main() {
+    
+    cin >> n;
+    
+    FOR(i,0,n){
+        int temp; scanf("%d", &temp);
+        if(temp == 0){
+            if(pq.empty()) printf("0\n");
+            else{
+                printf("%d\n", pq.top());
+                pq.pop();
+            }
+        }
+        else{
+            pq.push(temp);
+        }
+    }
+    
+    return 0;
+}
