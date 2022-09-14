e5f82bc9b5fb032298111ecf0d5da3a16b70a426
Park Taewoo <kauasltw@gmail.com>
2022-09-15 00:04:52 +0900

    Update 두 큐 합 같게 만들기.py

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,19 +1,25 @@
 from collections import deque
+
 def solution(queue1, queue2):
     answer = 0
-    queue1, queue2 = deque(queue1), deque(queue2)
-    sum1, sum2 = sum(queue1), sum(queue2)
     
-    for _ in range(3*len(queue1)):
-        if sum1 > sum2:
-            sum1 -= queue1[0]
-            sum2 += queue1[0]
+    l = len(queue1)
+    sum_q1, sum_q2 = sum(queue1), sum(queue2)
+    total = sum_q1 + sum_q2
+    
+    queue1, queue2 = deque(queue1), deque(queue2)
+    
+    while answer < l*4:
+        if sum_q1 > sum_q2:
+            sum_q1 -= queue1[0]
+            sum_q2 += queue1[0]
             queue2.append(queue1.popleft())
-        elif sum1 < sum2:
-            sum1 += queue2[0]
-            sum2 -= queue2[0]
+        elif sum_q1 < sum_q2:
+            sum_q2 -= queue2[0]
+            sum_q1 += queue2[0]
             queue1.append(queue2.popleft())
         else:
             return answer
         answer += 1
+    
     return -1
